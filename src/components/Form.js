b7cbc7291080aaba1139a1de8682569b73f6f30d
ESTIPAPPENHAIM <grosberg002@gmail.com>
2023-12-20 18:20:01 +0200

    context

--- src/components/Form.js
+++ src/components/Form.js
@@ -33,12 +33,12 @@ const Form = ({addAnimalToAnimalList}) => {
   return (
     <form onSubmit={addAnimal} className='form-container'>
       <div>
-     <input value={animal.name} onChange={handelAnimalNameChange} type='text' placeholder='Enter Animal Name'/>
-     <input value={animal.imageSrc} onChange={handelAnimalImageSrcChange} type='text' placeholder='Enter Image src'/>
-     <input value={animal.age} onChange={handelAnimalAgeChange} type='number' min={0}/>
+     <input name={animal.name} onChange={handelAnimalNameChange} type='text' placeholder='Enter Animal Name'/>
+     <input name={animal.imageSrc} onChange={handelAnimalImageSrcChange} type='text' placeholder='Enter Image src'/>
+     <input name={animal.age} onChange={handelAnimalAgeChange} type='number' min={0}/>
      </div>
      <div>
-     <Select animal={animal} handelAnimalTypeChange={handelAnimalTypeChange}/>
+     <Select name={animal} animal={animal} handelAnimalTypeChange={handelAnimalTypeChange}/>
      <button type='submit'>Add</button>
      </div>
      <span>add animal to the zoo</span>
